7680220186f7d84af602918c0a2bf145360d28b4
longo2002 <lombardo.fra2002@gmail.com>
2023-02-02 16:27:01 +0100

    Polish

--- js/app.js
+++ js/app.js
@@ -1,11 +1,30 @@
 console.log ("woks!")
-for (let i = 0; i < 100; i++) {
 
-    if ((i % 3 === 0 ) && (i % 5 === 0 )) {
-        console.log(i, 'FizzBuzz')
-    } else if (i % 3 === 0) {
-        console.log(i, 'Fizz')
-	} else if (i % 5 === 0) {
-        console.log(i, 'Buzz')
-	} 
-}
+let bottone = document.getElementById("bottone");
+let refresh = document.getElementById("refresh");
+
+bottone.addEventListener ( "click",
+
+    function myFunction() {
+        for (let i = 0; i < 100; i++) {
+
+            if ((i % 3 === 0 ) && (i % 5 === 0 )) {
+                console.log(i, 'FizzBuzz')
+            } else if (i % 3 === 0) {
+                console.log(i, 'Fizz')
+            } else if (i % 5 === 0) {
+                console.log(i, 'Buzz')
+            } 
+        }    
+        
+    }
+    
+
+)
+
+refresh.addEventListener ( "click",
+    function refreshFunction() {
+        window.location.reload(true)
+    }
+)
+
